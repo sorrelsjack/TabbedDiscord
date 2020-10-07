b92adf60fc412ffb02bae2658b0fe3a78249d31e
Jack Sorrels <sorrelsjack@gmail.com>
2020-10-07 17:11:51 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    // Create the browser window.
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+  
+    // and load the index.html of the app.
+    win.loadFile('index.html');
+  }
+  
+  app.whenReady().then(createWindow);
