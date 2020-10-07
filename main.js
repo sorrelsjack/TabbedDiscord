4b759ffd5e092a92186bae5d71dc2963ffc8a6c8
Jack Sorrels <sorrelsjack@gmail.com>
2020-10-07 17:53:08 -0400

    Support for env variables

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
+
 const electron = require('electron');
 const { app, BrowserWindow, BrowserView } = require('electron');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const baseDiscordUrl = 'https://discord.com/';
+
 const createWindow = () => {
-    // Create the browser window.
     const win = new BrowserWindow({
       width: 800,
       height: 600,
@@ -17,9 +20,9 @@ const createWindow = () => {
     const view = new BrowserView();
     win.setBrowserView(view);
     view.setBounds({ x: 0, y: 0, width: 800, height: 600});
-    view.webContents.loadURL('https://discord.com/');
+    view.webContents.loadURL(baseDiscordUrl);
   
-    // and load the index.html of the app.
+    //console.log(client.channels.cache.map(c => c.key))
     win.loadFile('index.html');
   }
   
